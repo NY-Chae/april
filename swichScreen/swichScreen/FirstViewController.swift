35126042dc6b93e2ed6f35a2798828385d8aa8ec
Nayeon Chae <cny1230@naver.com>
2024-04-18 08:46:05 +0900

    update

--- swichScreen/swichScreen/FirstViewController.swift
+++ swichScreen/swichScreen/FirstViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 
+
+
 class FirstViewController: UIViewController {
     var count = 0
+    
+    @IBOutlet weak var didTap: UIButton!
+   
     
     // 사용자가 버튼을 탭했을 때 호출되는 액션 메서드
     @IBAction func didTap(_ sender: Any) {
